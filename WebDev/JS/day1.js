0b2b6429bc2f025bfaac2e32d475dbd285148e34
PRANTOR DAS <prantor.college@gmail.com>
2026-02-03 15:01:14 +0530

    fuction, array and objects in js

--- WebDev/JS/day1.js
+++ WebDev/JS/day1.js
@@ -25,8 +25,68 @@
 // function greet(name) {
 //     return "Hello, " + name;
 // }
-
+//
 // // Function call
 // let message = greet("John"); // "Hello, John"
 // console.log(message);
+// 
+// Assignment
+// function isLegal(name, age) {
+//   if (age >= 18) {
+//     console.log(name + " is allowed to vote");
+//   } else {
+//     console.log(name + " is not allowed to vote");
+//   }
+// }
+// 
+// isLegal("Prantor", 20);
+// isLegal("Shivam", 17);
 
+// Objects
+// An object in JavaScript is a collection of`key-value pairs`, 
+// where each `key` is a string and each `value` can be any valid JavaScript data type, including another object.
+// let user = {
+// 	name: "Harkirat",
+// 	age: 19
+// }
+// 
+// console.log("Harkirats age is " + user.age);
+
+// Arrays
+// Arrays let you group data together
+// const users = ["harkirat", "raman", "prantor", "diljeet"];
+// const totalUsers = users.length;
+// const firstUser = users[0];
+// 
+// console.log(`Total Users: ${totalUsers} \nFirst User: ${firstUser}`);
+
+// Array of Objects
+// We can have more complex objects, for example an array of objects
+// const users = [{
+// 		name: "Harkirat",
+// 		age: 21
+// 	}, {
+// 		name: "raman",
+// 		age: 22
+// 	}
+// ];
+// 
+// const user1 = users[0] 
+// const user1Age = users[0].age
+// console.log(user1);
+// console.log(user1Age);
+
+// Object of Objects
+// We can have an even more complex object (object of objects)
+// const user1 = {
+// 	name: "harkirat",
+// 	age: 19,
+// 	address: {
+// 		city: "Delhi",
+// 		country: "India",
+// 		address: "1122 DLF"
+// 	}
+// }
+// 
+// const city = user1.address.city;
+// console.log(city);
