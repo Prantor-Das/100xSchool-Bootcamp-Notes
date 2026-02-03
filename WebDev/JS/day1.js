3fb7837bed987d3c00d66f5ed7f30819c28e3c15
PRANTOR DAS <prantor.college@gmail.com>
2026-02-03 10:57:48 +0530

    Basic JS

---
+++ WebDev/JS/day1.js
@@ -0,0 +1,32 @@
+// console.log("Hi There");
+
+// JS is Dynamically Typed
+// var a = 1;
+// a = "harkirat";
+// a = true;
+
+// console.log(a);
+
+// Variable
+// let name = "John";     // Variable that can be reassigned
+// const age = 30;        // Constant variable that cannot be reassigned
+// var isStudent = true;  // Older way to declare variables, function-scoped
+
+// Assignment
+// let color = "black";
+// let height = 176;
+// let likePizza = false;
+// console.log(`Color: ${color} \nHeight: ${height} \nLike Pizza: ${likePizza}`)
+
+// Functions
+// code block used to run a repetitive task
+// 
+// Function declaration
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+
+// // Function call
+// let message = greet("John"); // "Hello, John"
+// console.log(message);
+
